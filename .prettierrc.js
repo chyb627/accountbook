13703ca864c4e6897817b8cf6ddd8a967b34236b
cha <chyb627@google.com>
2022-12-05 17:15:58 +0900

    npx react-native init accountbook --template react-native-template-typescript

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  // https://prettier.io/docs/en/configuration.html 참고
+  bracketSpacing: true,
+  bracketSameLine: true,
+  semi: true,
+  printWidth: 100,
+  singleQuote: true,
+  trailingComma: 'all',
+};
